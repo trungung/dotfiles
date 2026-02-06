47b9dc943a09de8b6bcdd390b0704ef53e9cdaf8
trungung <ungkientrung@gmail.com>
2026-02-07 01:43:58 +0200

    add mason and lspconfig

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -4,6 +4,8 @@ vim.pack.add({
   'https://github.com/echasnovski/mini.nvim',
   'https://github.com/nvim-lua/plenary.nvim',
   'https://github.com/ibhagwan/fzf-lua',
+  'https://github.com/mason-org/mason.nvim',
+  'https://github.com/neovim/nvim-lspconfig',
   'https://github.com/nvim-treesitter/nvim-treesitter',
   'https://github.com/lewis6991/gitsigns.nvim',
   'https://github.com/stevearc/oil.nvim',
@@ -70,6 +72,11 @@ require('gitsigns').setup({
 require('fzf-lua').setup({})
 map('n', '<leader>ff', function() require('fzf-lua').files() end, 'Find files')
 map('n', '<leader>fg', function() require('fzf-lua').live_grep() end, 'Live grep')
+
+-- LSP (Neovim 0.11+)
+-- Install language servers via :Mason, then enable them here.
+require('mason').setup({})
+vim.lsp.enable({ 'lua_ls', 'ts_ls', 'pyright', 'gopls', 'csharp_ls' })
 
 -- treesitter
 -- No need to call setup for nvim-treesitter to work using default values.
