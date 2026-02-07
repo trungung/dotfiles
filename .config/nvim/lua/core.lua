0864ea1ecf977ad1ceec1a80c6e431559e05985f
trungung <ungkientrung@gmail.com>
2026-02-07 19:57:21 +0200

    add diagnostic

--- .config/nvim/lua/core.lua
+++ .config/nvim/lua/core.lua
@@ -19,3 +19,17 @@ vim.g.loaded_netrw = 1 -- disable netrw to avoid conflict with Oil
 vim.g.loaded_netrwPlugin = 1 -- disable netrw to avoid conflict with Oil
 
 vim.o.termguicolors=true
+
+vim.diagnostic.config({
+  virtual_text = true,
+  virtual_lines = false,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = " ",
+      [vim.diagnostic.severity.WARN] = " ",
+      [vim.diagnostic.severity.HINT] = "󰠠 ",
+      [vim.diagnostic.severity.INFO] = " ",
+    },
+  },
+  }
+)
